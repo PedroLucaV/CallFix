0382ed3e2345dad473a66743ed8d4b6883b1acf4
Pedro Lucas <izielucas2016@gmail.com>
2025-02-17 10:36:25 -0300

    FEAT/Added middleware to form requisitions

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, { json, Request, Response, urlencoded } from 'express';
 
 const app = express();
+
+app.use(json());
+app.use(urlencoded({extended: true}));
 
 app.get('/', (req: Request, res: Response) => {
     res.end("Hello World");
     console.log("ENDPOINT - GET '/'");
-    
+
 })
 
 export default app; 
